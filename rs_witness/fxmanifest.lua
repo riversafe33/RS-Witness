aaddc208bae1c0c470f1fe6657a02fc85ada2524
riversafe33 <riversafe33@outlook.com>
2025-10-13 11:56:48 +0200

    Update fxmanifest.lua

--- rs_witness/fxmanifest.lua
+++ rs_witness/fxmanifest.lua
@@ -1,11 +1,11 @@
-version '1.0.0'
-author 'riversafe'
-
 fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 lua54 'yes'
+
+author 'riversafe'
+version '1.0.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
